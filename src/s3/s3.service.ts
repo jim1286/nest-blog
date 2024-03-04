cd08d4eb61c908510e2f0b6fe1bf2ea2866052ff
jimin <jimin1286@gmail.com>
2024-03-04 21:18:22 +0900

    [Fix] fix

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -22,9 +22,12 @@ export class S3Service {
     file: Express.Multer.File, // 업로드할 파일
     ext: string, // 파일 확장자
   ) {
+    const region = this.configService.get('S3_REGION');
+    const bucketName = this.configService.get('S3_REGION');
+
     // AWS S3에 이미지 업로드 명령을 생성합니다. 파일 이름, 파일 버퍼, 파일 접근 권한, 파일 타입 등을 설정합니다.
     const command = new PutObjectCommand({
-      Bucket: this.configService.get('S3_BUCKET_NAME'), // S3 버킷 이름
+      Bucket: bucketName, // S3 버킷 이름
       Key: fileName, // 업로드될 파일의 이름
       Body: file.buffer, // 업로드할 파일
       ACL: 'public-read', // 파일 접근 권한
@@ -35,6 +38,6 @@ export class S3Service {
     await this.s3Client.send(command);
 
     // 업로드된 이미지의 URL을 반환합니다.
-    return `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}/${fileName}`;
+    return `https://s3.${region}.amazonaws.com/${bucketName}/${fileName}`;
   }
 }
